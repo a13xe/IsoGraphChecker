c29f3bca4a5f7e08fc102205d5560cd87b8b9a8a
a13xe <77492646+a13xe@users.noreply.github.com>
2024-05-19 23:02:42 +0300

    layout fix

--- GUI.py
+++ GUI.py
@@ -7,11 +7,19 @@ from tkinter import filedialog, messagebox
 from networkx.algorithms import isomorphism
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+# High DPI settings fix
+try:
+    from ctypes import windll
+    windll.shcore.SetProcessDpiAwareness(1)
+except:
+    pass
+
 class LogColors:
     SUCCESS = "#28a745"
     WARNING = "#ffc107"
     ERROR = "#dc3545"
-    TEXT = "#000000"
+    TEXT = "#ffffff"
+    INFO = "#0a9bff"
     
 class GraphDisplay:
     def __init__(self, master):
@@ -25,8 +33,8 @@ class GraphDisplay:
         num_edges = len(graph.edges())
         
         if num_nodes > max_nodes or num_edges > max_edges:
-            label = tk.Label(frame, text="The graph is too big to open.")
-            label.grid(row=1, column=0, sticky="nsew")
+            label = tk.Label(frame, text="Граф слишком объемный для его отображения.")
+            label.grid(row=0, column=0, sticky="nsew")
         else:
             fig, ax = plt.subplots(figsize=(5, 5))
             pos = nx.spring_layout(graph)
@@ -44,9 +52,9 @@ class GraphIsomorphismCheckerApp(ctk.CTk):
 
         # Store references to after callbacks
         self.after_callbacks = []
-        frame_width = 500
-        frame_height = 500
-        log_height = 360
+        graph_frame_width = graph_frame_height = 500
+        controls_width = 200
+        log_height = 350
 
         # Configure grid layout
         self.grid_columnconfigure(1, weight=1)
@@ -54,18 +62,20 @@ class GraphIsomorphismCheckerApp(ctk.CTk):
         self.grid_rowconfigure(0, weight=1)
 
         # Frames for layout
-        self.frame_controls = ctk.CTkFrame(self)
+        self.frame_controls = ctk.CTkFrame(self, width=controls_width+20)
         self.frame_controls.grid(row=0, column=0, padx=(10,0), pady=(10,10), sticky="nsew")
-        self.frame_graphs = ctk.CTkFrame(self)
+        self.frame_controls.grid_propagate(False)
+        self.frame_graphs = ctk.CTkFrame(self, width=graph_frame_width+20, height=graph_frame_height+20)
         self.frame_graphs.grid(row=0, column=1, padx=(10,10), pady=(10,10), sticky="nsew")
+        self.frame_graphs.grid_propagate(False)
         
         # Frames
-        self.frame_graph1 = ctk.CTkFrame(self.frame_graphs, width=frame_width, height=frame_height)
+        self.frame_graph1 = ctk.CTkFrame(self.frame_graphs, width=graph_frame_width, height=graph_frame_height)
         self.frame_graph1.grid(row=0, column=0, padx=(10,5), pady=(10,10), sticky="nsew")
         self.frame_graph1.grid_rowconfigure(0, weight=1)
         self.frame_graph1.grid_columnconfigure(0, weight=1)
         self.frame_graph1.grid_propagate(False)
-        self.frame_graph2 = ctk.CTkFrame(self.frame_graphs, width=frame_width, height=frame_height)
+        self.frame_graph2 = ctk.CTkFrame(self.frame_graphs, width=graph_frame_width, height=graph_frame_height)
         self.frame_graph2.grid(row=0, column=1, padx=(5,10), pady=(10,10), sticky="nsew")
         self.frame_graph2.grid_rowconfigure(0, weight=1)
         self.frame_graph2.grid_columnconfigure(0, weight=1)
@@ -75,13 +85,14 @@ class GraphIsomorphismCheckerApp(ctk.CTk):
         self.button_load1 = ctk.CTkButton(self.frame_controls, text="Загрузить 1 схему (JSON)", command=self.load_graph1)
         self.button_load1.grid(row=0, column=0, padx=(10,10), pady=(10,10), sticky="nsew")
         self.button_load2 = ctk.CTkButton(self.frame_controls, text="Загрузить 2 схему (JSON)", command=self.load_graph2)
-        self.button_load2.grid(row=1, column=0, padx=(10,10), pady=(10,10), sticky="nsew")
-        self.button_check = ctk.CTkButton(self.frame_controls, text="Проверить на изоморфизм", command=self.check_isomorphism)
-        self.button_check.grid(row=2, column=0, padx=(10,10), pady=(10,10), sticky="nsew")
+        self.button_load2.grid(row=1, column=0, padx=(10,10), pady=(0,10), sticky="nsew")
+        self.combobox_1 = ctk.CTkComboBox(self.frame_controls, values=["", "", ""])
+        self.combobox_1.grid(row=2, column=0, padx=(10,10), pady=(0,10), sticky="nsew")
+        self.button_check = ctk.CTkButton(self.frame_controls, text="Проверить на изоморфизм", command=self.check_isomorphism, fg_color="green")
+        self.button_check.grid(row=3, column=0, padx=(10,10), pady=(0,10), sticky="nsew")
         self.log_widget = ctk.CTkTextbox(self.frame_controls, wrap="word", state='disabled', height=log_height)
-        self.log_widget.grid(row=3, column=0, padx=(10,10), pady=(10,10), sticky="nsew")
+        self.log_widget.grid(row=4, column=0, padx=(10,10), pady=(0,10), sticky="nsew")
         self.log_widget.grid_propagate(False)
-        
         self.label_graph1 = ctk.CTkLabel(self.frame_graph1, text="Чтобы загрузить граф схемы 1\n" + 
                                                                  "для дальнейшей проверки на изоморфизм, \n" +
                                                                  "нажмите на кнопку «Загрузить схему 1 (JSON)».")
@@ -92,16 +103,13 @@ class GraphIsomorphismCheckerApp(ctk.CTk):
         self.label_graph2.grid(row=0, column=0, sticky="nsew")
 
         # Configure log text tags for colors
-        self.log_widget.tag_config("SUCCESS", foreground=LogColors.SUCCESS)
+        self.log_widget.tag_config("SUCCESS", foreground=LogColors.SUCCESS) 
         self.log_widget.tag_config("ERROR", foreground=LogColors.ERROR)
         self.log_widget.tag_config("TEXT", foreground=LogColors.TEXT)
         self.log_widget.tag_config("WARNING", foreground=LogColors.WARNING)
-        # Examples
-        # self.log("This is a success message.", "SUCCESS") 
-        # self.log("This is an error message.", "ERROR")
-        # self.log("This is a normal text message.", "TEXT")
-        # self.log("This is a warning message.", "WARNING"
-
+        self.log_widget.tag_config("INFO", foreground=LogColors.INFO)
+        self.log("Данная программа предназначена для проверки графов на изоморфность", "INFO")
+        self.log("Для начала работы сперва загрузите два графа с помощью кнопок выше.", "TEXT")
         self.graph1 = None
         self.graph2 = None
 
@@ -112,9 +120,9 @@ class GraphIsomorphismCheckerApp(ctk.CTk):
                 data = json.load(f)
                 self.graph1 = self.create_graph(data)
                 GraphDisplay.display_graph(self, self.graph1, self.frame_graph1)
-            self.log(f"Graph 1 loaded from {file_path}", "SUCCESS")
+            self.log(f"Граф 1 загружен из файла {file_path}", "SUCCESS")
         else:
-            self.log("Graph 1 loading cancelled", "WARNING")
+            self.log("Загрузка графа 1 отменена.", "WARNING")
 
     def load_graph2(self):
         file_path = filedialog.askopenfilename(filetypes=[("JSON files", "*.json")])
@@ -123,9 +131,9 @@ class GraphIsomorphismCheckerApp(ctk.CTk):
                 data = json.load(f)
                 self.graph2 = self.create_graph(data)
                 GraphDisplay.display_graph(self, self.graph2, self.frame_graph2)
-            self.log(f"Graph 2 loaded from {file_path}", "SUCCESS")
+            self.log(f"Граф 2 загружен из файла {file_path}", "SUCCESS")
         else:
-            self.log("Graph 2 loading cancelled", "WARNING")
+            self.log("Загрузка графа 2 отменена.", "WARNING")
 
     def create_graph(self, data):
         graph = nx.Graph()
@@ -136,16 +144,16 @@ class GraphIsomorphismCheckerApp(ctk.CTk):
         return graph
 
     def check_isomorphism(self):
-        self.log("Checking isomorphism...", "TEXT")
+        self.log("Проверка на изоморфизм...", "TEXT")
         if self.graph1 is None or self.graph2 is None:
-            self.log("Both graphs need to be loaded first.", "ERROR")
+            self.log("Оба графа должны быть сперва загружены!", "ERROR")
             return
         # GM = nx.isomorphism.GraphMatcher(self.graph1, self.graph2)
         GM = isomorphism.GraphMatcher(self.graph1, self.graph2)
         if GM.is_isomorphic():
-            self.log("Graphs are isomorphic!", "SUCCESS")
+            self.log("Графы изоморфны!", "SUCCESS")
         else:
-            self.log("Graphs are not isomorphic.", "ERROR")
+            self.log("Графы не изоморфны!", "ERROR")
 
     def log(self, message, tag):
         self.log_widget.configure(state='normal')
